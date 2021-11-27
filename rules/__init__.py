8de0c60a6da84dee941d3e10032599338a3feaaa
Boris Bredikhin <boris00043@yandex.ru>
2021-11-27 17:43:27 +0500

    пример базы правил

--- rules/__init__.py
+++ rules/__init__.py
@@ -4,9 +4,11 @@ from typing import Any, Tuple, TypedDict
 Пакет содержит базовые классы и функции для продукционных правил
 """
 
+
 class KnowledgeBase:
     rules: list['Rule']
 
+
 class Rule(TypedDict):
-    antecedent: list # условие
-    consequen: Tuple[str, Any] # действие
+    antecedent: list  # условие
+    consequent: Tuple[str, Any]  # действие
